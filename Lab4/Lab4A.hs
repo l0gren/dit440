402b8a284818fe2e79ba68fdd6aaffa650f50a1a
l0gren <emil.logren@gmail.com>
2020-10-15 13:11:15 +0200

    Finish Lab4A

--- Lab4/Lab4A.hs
+++ Lab4/Lab4A.hs
@@ -1,5 +1,5 @@
 -- Authors: Emil Logren & Daniel Ericsson
--- Date: 
+-- Date: 2020-10-15
 
 import Poly
 import Test.QuickCheck
@@ -10,33 +10,25 @@ data BinOp = AddOp | MulOp
 
 --------------------------------------------------------------------------------
 -- * A1
--- Define a data type Expr which represents three kinds of expression:
--- binary operators (use BinOp as a helper type) applied to two expressions,
--- numbers (use Int), and exponentiation x^n.
--- Note that since we consider expressions containing just a single variable,
--- x, your data type should not use String or Char anywhere, since this is
--- not needed.
-
--- Expression either an integer, an operator, or a variable (X) raised to an int
+-- Expression either an integer, a binary operation on two expressions, 
+-- or a variable (X) raised to a non-negative Int
 data Expr = Cons Int | Op BinOp Expr Expr | X Int
 
 
 --------------------------------------------------------------------------------
 -- * A2
--- Define the data type invariant that checks that exponents are never negative
+-- | Ensures no negative exponents occur in expressions
 prop_Expr :: Expr -> Bool
 prop_Expr e = case e of
   X n        -> n >= 0
   Op _ e1 e2 -> prop_Expr e1 && prop_Expr e2
-  _            -> True
+  _          -> True
 
 
 --------------------------------------------------------------------------------
 -- * A3
--- Make Expr an instance of Show (along the lines of the example in the lecture)
--- You can use Haskell notation for powers: x^2
--- You should show x^1 as just x. 
-
+-- | Rules for showing an expression, that include eliminating superfluous symbols,
+-- and correctly parenthesising according to precedence of evaluation
 showExpr :: Expr -> String
 showExpr e = case e of 
   Cons c         -> show c
@@ -60,15 +52,8 @@ instance Show Expr where
 
 --------------------------------------------------------------------------------
 -- * A4
--- Make Expr an instance of Arbitrary.
--- Now you can check the data type invariant that you defined in A3 using
--- quickCheck
-
--- (Optional)
--- Add a definition of function shrink :: Expr -> [Expr] to Arbitrary
--- which gives hints to quickCheck on possible smaller expressions that it
--- could use to find a smaller counterexample for failing tests
-
+-- | Generates random expressions by combining numerical operations and variable
+-- with either addition or multiplication, with a given size n
 rExpr :: Int -> Gen Expr
 rExpr n = frequency [(1, genOp), (1, genExpo), (n, genExpr)]
   where
@@ -79,8 +64,8 @@ rExpr n = frequency [(1, genOp), (1, genExpo), (n, genExpr)]
       return (Op op (Cons x) (Cons y))
 
     genExpo = do 
-      n <- choose (0,10)
-      return (X n)
+      m <- choose (0,10)
+      return (X m)
 
     genExpr = let m = n `div` 4 in do
       op <- elements [AddOp, MulOp]
@@ -88,82 +73,71 @@ rExpr n = frequency [(1, genOp), (1, genExpo), (n, genExpr)]
       e2 <- rExpr m
       return (Op op e1 e2)
 
-
 instance Arbitrary Expr
   where arbitrary = sized rExpr
 
-
 --------------------------------------------------------------------------------
 -- * A5
--- Define the eval function which takes a value for x and an expression and
--- evaluates it
-
+-- Evaluates an expression with a given value x
 eval :: Int -> Expr -> Int
 eval x expr = case expr of
   Cons c         -> c
   Op AddOp e1 e2 -> eval x e1 + eval x e2
   Op MulOp e1 e2 -> eval x e1 * eval x e2
   X n            -> x^n
-  
-
 
 --------------------------------------------------------------------------------
 -- * A6
--- Define
+-- | Converts an expression into a polynomial by adding and multiplying
+-- the polynomials of each component of the expression
 exprToPoly :: Expr -> Poly
--- Which converts an expression into a polynomial.
--- Here it is important to think recursively to just solve the bigger problem
--- by solving the smaller problems and combining them in the right way. 
-
 exprToPoly expr = case expr of 
   Cons c         -> fromList [c]
   X n            -> fromList (1 : replicate n 0)
   Op AddOp e1 e2 -> exprToPoly e1 + exprToPoly e2 
   Op MulOp e1 e2 -> exprToPoly e1 * exprToPoly e2
 
--- Define (and check) prop_exprToPoly, which checks that evaluating the
--- polynomial you get from exprToPoly gives the same answer as evaluating
--- the expression
+-- | Ensures that the evaluation of an expression is equal to the evaluation
+-- of that expression converted into a polynomial
 prop_exprToPoly :: Int -> Expr -> Bool
 prop_exprToPoly x expr = eval x expr == evalPoly x (exprToPoly expr)  
 
 --------------------------------------------------------------------------------
 -- * A7
--- Now define the function going in the other direction, 
+-- | Converts a polynomial into an expression by first creating a list of its
+-- constants and exponents, and then reducing these into a single expression sans junk
 polyToExpr :: Poly -> Expr
-polyToExpr poly = let constants = toList poly in case constants of
-  []     -> Cons 0
-  [c]    -> Cons c
-  (c:cs) -> Op AddOp (toExpr c (length cs)) (polyToExpr $ fromList cs)
+polyToExpr poly = let constants = (reverse . toList) poly in
+  foldr termToExpr (Cons 0) (zip constants [0..(length constants - 1)])
     where
-      toExpr c n
-        | c == 1    = (X n)
-        | otherwise = (Op MulOp (Cons c) (X n))
+      -- A term (Int, Int) is a constant and an exponent
+      termToExpr :: (Int, Int) -> Expr -> Expr
+      termToExpr (0, _) e        = e
+      termToExpr (1, n) e        = Op AddOp (X n) e
+      termToExpr (c, n) (Cons 0) = Op MulOp (Cons c) (X n)
+      termToExpr (c, n) e        = Op AddOp (Op MulOp (Cons c) (X n)) e  
 
-
--- Write (and check) a quickCheck property for this function similar to
--- question 6. 
+-- | Ensures that the evaluation of a polynomial is equal to
+-- the evaluation of that polynomial converted into an expression
 prop_polyToExpr :: Int -> Poly -> Bool
 prop_polyToExpr x poly = evalPoly x poly == eval x (polyToExpr poly)
 
 --------------------------------------------------------------------------------
 -- * A8
--- Write a function
+-- | Simplifies an expression by converting it to a canonical polynomial
+-- and then back to an expression
 simplify :: Expr -> Expr
--- which simplifies an expression by converting it to a polynomial
--- and back again
 simplify expr = (polyToExpr . exprToPoly) expr 
 
 --------------------------------------------------------------------------------
 -- * A9
--- Write a quickCheck property
+-- | Ensures no additions by 0, and no multiplications by 0 or 1
 prop_noJunk :: Expr -> Bool
-
---that checks that a simplified expression does not contain any "junk":
---where junk is defined to be multiplication by one or zero,
---addition of zero, addition or multiplication of numbers, or x to the
---power zero. (You may need to fix A7)
-
-prop_noJunk = undefined
+prop_noJunk (Op AddOp (Cons c1) _)  = c1 /= 0
+prop_noJunk (Op AddOp _ (Cons c2))  = c2 /= 0
+prop_noJunk (Op MulOp (Cons c1) _)  = c1 /= 0 && c1 /= 1
+prop_noJunk (Op MulOp _ (Cons c2))  = c2 /= 0 && c2 /= 1
+prop_noJunk (Op _ e1 e2)            = prop_noJunk e1 && prop_noJunk e2
+prop_noJunk _                       = True
 
 --------------------------------------------------------------------------------
